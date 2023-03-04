c3e055da06c0f58c7784f81534d572af05f39dda
Mustafaa71 <m.almeer71@gmail.com>
2023-03-04 21:21:59 +0300

    Some changes

--- lib/components/onboarding_contents.dart
+++ lib/components/onboarding_contents.dart
@@ -13,19 +13,17 @@ class OnboardingContents {
 List<OnboardingContents> contents = [
   OnboardingContents(
     title: "Teams Maker",
-    image: "/Users/rahaf/final-project/assets/images/codetech-logo.png",
-    desc:
-        "An app to generate team groups based on a list of members and their preferences.",
+    image: "/Users/rahaf/Desktop/flutter-2/teams_maker_rahaf_copy/assets/images/team.png",
+    desc: "An app to generate team groups based on a list of members and their preferences.",
   ),
   OnboardingContents(
     title: "Hadi Albinsaad",
-    image: "/Users/rahaf/final-project/assets/images/codetech-logo.png",
-    desc:
-        "The leader who took the first step, The owner and developer of the idea.",
+    image: "/Users/rahaf/Desktop/flutter-2/teams_maker_rahaf_copy/assets/images/hh.jpg",
+    desc: "The leader who took the first step, The owner and developer of the idea.",
   ),
   OnboardingContents(
     title: "Rahaf Alharbi",
-    image: "/Users/rahaf/final-project/assets/images/codetech-logo.png",
+    image: "/Users/rahaf/Desktop/flutter-2/teams_maker_rahaf_copy/assets/images/Rahaf.png",
     desc: "The legend who made the design of the app.",
   ),
 ];
