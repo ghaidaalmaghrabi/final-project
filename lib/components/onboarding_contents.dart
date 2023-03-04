b41de9bbc595a7291509d8f2da80847ef25d2343
Mustafaa71 <m.almeer71@gmail.com>
2023-03-04 21:36:29 +0300

    Assets fix

--- lib/components/onboarding_contents.dart
+++ lib/components/onboarding_contents.dart
@@ -12,18 +12,18 @@ class OnboardingContents {
 
 List<OnboardingContents> contents = [
   OnboardingContents(
-    title: "Teams Maker",
-    image: "/Users/rahaf/Desktop/flutter-2/teams_maker_rahaf_copy/assets/images/team.png",
-    desc: "An app to generate team groups based on a list of members and their preferences.",
+    title: 'Teams Maker',
+    image: '/Users/mustafa/Desktop/tuwaiq-flutter/Our-Final-Project/final-project/assets/images/codetech-logo.png',
+    desc: 'An app to generate team groups based on a list of members and their preferences.',
   ),
   OnboardingContents(
-    title: "Hadi Albinsaad",
-    image: "/Users/rahaf/Desktop/flutter-2/teams_maker_rahaf_copy/assets/images/hh.jpg",
-    desc: "The leader who took the first step, The owner and developer of the idea.",
+    title: 'Hadi Albinsaad',
+    image: '/Users/mustafa/Desktop/tuwaiq-flutter/Our-Final-Project/final-project/assets/images/codetech-logo.png',
+    desc: 'The leader who took the first step, The owner and developer of the idea.',
   ),
   OnboardingContents(
-    title: "Rahaf Alharbi",
-    image: "/Users/rahaf/Desktop/flutter-2/teams_maker_rahaf_copy/assets/images/Rahaf.png",
-    desc: "The legend who made the design of the app.",
+    title: 'Rahaf Alharbi',
+    image: '/Users/mustafa/Desktop/tuwaiq-flutter/Our-Final-Project/final-project/assets/images/codetech-logo.png',
+    desc: 'The legend who made the design of the app.',
   ),
 ];
