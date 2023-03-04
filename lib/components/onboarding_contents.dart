d3dd817a6ac7c23ef45927bd675afb0df14e04db
Rahaf Alharbi <rahaf.alharbi.cs@gmail.com>
2023-03-04 21:19:27 +0300

    Add assets + add logo

--- lib/components/onboarding_contents.dart
+++ lib/components/onboarding_contents.dart
@@ -13,22 +13,19 @@ class OnboardingContents {
 List<OnboardingContents> contents = [
   OnboardingContents(
     title: "Teams Maker",
-    image:
-        "/Users/rahaf/Desktop/flutter-2/teams_maker_rahaf_copy/assets/images/team.png",
+    image: "/Users/rahaf/final-project/assets/images/codetech-logo.png",
     desc:
         "An app to generate team groups based on a list of members and their preferences.",
   ),
   OnboardingContents(
     title: "Hadi Albinsaad",
-    image:
-        "/Users/rahaf/Desktop/flutter-2/teams_maker_rahaf_copy/assets/images/hh.jpg",
+    image: "/Users/rahaf/final-project/assets/images/codetech-logo.png",
     desc:
         "The leader who took the first step, The owner and developer of the idea.",
   ),
   OnboardingContents(
     title: "Rahaf Alharbi",
-    image:
-        "/Users/rahaf/Desktop/flutter-2/teams_maker_rahaf_copy/assets/images/Rahaf.png",
+    image: "/Users/rahaf/final-project/assets/images/codetech-logo.png",
     desc: "The legend who made the design of the app.",
   ),
 ];
