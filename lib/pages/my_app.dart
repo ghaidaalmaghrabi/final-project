2ec8ccaf86bfd8c5327fd5a4931bde8d44bbadee
Mustafaa71 <m.almeer71@gmail.com>
2023-03-05 14:54:35 +0300

    some changes

--- lib/pages/my_app.dart
+++ lib/pages/my_app.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: VideoSelectorWidget(),
+      home: const HomePage(),
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
       debugShowCheckedModeBanner: false,
