08c749dd534bcfcd82cbfb6c0ed27ee680507f9e
ghaidaalmaghrabi <ghaidaa.almaghrabi@gmail.com>
2023-03-05 14:34:02 +0300

    resolving error

--- lib/pages/my_app.dart
+++ lib/pages/my_app.dart
@@ -1,4 +1,4 @@
-import 'package:final_project/pages/profile_page.dart';
+import 'package:final_project/pages/onboarding.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -7,8 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const ProfilePage(),
-      // home: const onboarding(),
+      home: const onboarding(),
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
       debugShowCheckedModeBanner: false,
