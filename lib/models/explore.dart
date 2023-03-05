891fbd69bdf8fb57f1509617ea3d66a90776d72f
Mustafaa71 <m.almeer71@gmail.com>
2023-03-05 14:06:40 +0300

    some fixes

--- lib/models/explore.dart
+++ lib/models/explore.dart
@@ -17,3 +17,33 @@ class Explore {
     );
   }
 }
+
+class UserInformation {
+  final String phoneNumber;
+  final String gitHubLink;
+  final String linkedInLink;
+
+  UserInformation({
+    required this.phoneNumber,
+    required this.gitHubLink,
+    required this.linkedInLink,
+  });
+
+  /// This method is used to convert the json data to the UserInformation model.
+  factory UserInformation.fromJson(Map<String, dynamic> json) {
+    return UserInformation(
+      phoneNumber: json['phoneNumber'],
+      gitHubLink: json['gitHubLink'],
+      linkedInLink: json['linkedInLink'],
+    );
+  }
+
+  /// This method is used to convert the UserInformation model to the json data.
+  Map<String, dynamic> toJson() {
+    return {
+      'phoneNumber': phoneNumber,
+      'gitHubLink': gitHubLink,
+      'linkedInLink': linkedInLink,
+    };
+  }
+}
--- lib/pages/developers_list_page.dart
+++ lib/pages/developers_list_page.dart
@@ -1,1 +1,63 @@
+import 'package:flutter/material.dart';
 
+class DevelopersListPage extends StatefulWidget {
+  const DevelopersListPage({super.key});
+
+  @override
+  State<DevelopersListPage> createState() => _DevelopersListPageState();
+}
+
+class _DevelopersListPageState extends State<DevelopersListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('مطورين فلاتر'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView(
+          children: [
+            const Placeholder(
+              fallbackHeight: 200,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              decoration: const BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text('4#'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const [
+                      Text(
+                        'مايا',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Icon(Icons.circle, size: 40.0),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
