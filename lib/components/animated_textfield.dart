d96885eac43f65f22267b8aca7b11692f07942a0
Rahaf Alharbi <rahaf.alharbi.cs@gmail.com>
2023-03-06 19:17:12 +0300

    Design add new project page

---
+++ lib/components/animated_textfield.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+import 'custom_animate_border.dart';
+
+class AnimatedTextField extends StatefulWidget {
+  final String label;
+  final Widget? suffix;
+  const AnimatedTextField({Key? key, required this.label, required this.suffix})
+      : super(key: key);
+
+  @override
+  State<AnimatedTextField> createState() => _AnimatedTextFieldState();
+}
+
+class _AnimatedTextFieldState extends State<AnimatedTextField>
+    with SingleTickerProviderStateMixin {
+  AnimationController? controller;
+  late Animation<double> alpha;
+  final focusNode = FocusNode();
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 400),
+    );
+    final Animation<double> curve =
+        CurvedAnimation(parent: controller!, curve: Curves.easeInOut);
+    alpha = Tween(begin: 0.0, end: 1.0).animate(curve);
+
+    // controller?.forward();
+    controller?.addListener(() {
+      setState(() {});
+    });
+    focusNode.addListener(() {
+      if (focusNode.hasFocus) {
+        controller?.forward();
+      } else {
+        controller?.reverse();
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.blueGrey.shade300),
+          borderRadius: BorderRadius.all(Radius.circular(8))),
+      child: Theme(
+        data: ThemeData(
+          colorScheme: Theme.of(context).colorScheme.copyWith(
+                primary: Colors.cyan,
+              ),
+        ),
+        child: CustomPaint(
+          painter: CustomAnimateBorder(alpha.value),
+          child: TextField(
+            focusNode: focusNode,
+            decoration: InputDecoration(
+              //hintStyle: TextStyle(),
+              label: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(widget.label),
+                ],
+              ),
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              border: InputBorder.none,
+            ),
+            textAlign: TextAlign.end,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/add_new_project_page.dart
+++ lib/pages/add_new_project_page.dart
@@ -1,11 +1,18 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:final_project/models/explore.dart';
+import 'package:final_project/pages/settings.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animated_button/flutter_animated_button.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:video_player/video_player.dart';
+
+import '../components/animated_textfield.dart';
+import '../components/ct_elevatedButton.dart';
+import '../components/ct_textfield_title.dart';
 
 class AddNewProjectPage extends StatefulWidget {
   const AddNewProjectPage({super.key});
@@ -53,7 +60,9 @@ class _AddNewProjectPageState extends State<AddNewProjectPage> {
 
     final fileName = _selectedVideo!.path.split('/').last;
     final bytes = await _selectedVideo!.readAsBytes();
-    final response = await supabase.storage.from('demo-vid').uploadBinary('videos/$fileName', bytes);
+    final response = await supabase.storage
+        .from('demo-vid')
+        .uploadBinary('videos/$fileName', bytes);
   }
 
   ///
@@ -72,83 +81,180 @@ class _AddNewProjectPageState extends State<AddNewProjectPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('أضف مشروع جديد'),
-        actions: const [
-          Icon(Icons.close),
-          SizedBox(
-            width: 14,
-          ),
+        leading: InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const SettingsPage(),
+              ),
+            );
+          },
+          child: const Icon(Icons.menu, color: Colors.grey),
+        ),
+        automaticallyImplyLeading: false,
+        title: Image.asset('assets/images/LogoName.png', height: 50),
+        actions: [
+          Image.asset('assets/images/LogoPic.png', width: 50, height: 50),
+          const SizedBox(width: 10),
         ],
+        backgroundColor: Colors.white,
         centerTitle: true,
       ),
-      body: Column(
+      body: ListView(
         children: [
-          TextField(
-            controller: nameController,
-            decoration: const InputDecoration(
-              hintText: 'الاسم',
-              filled: true,
-              fillColor: Colors.white,
-            ),
-          ),
-          const SizedBox(height: 40),
-          TextField(
-            controller: descriptionController,
-            decoration: const InputDecoration(
-              hintText: 'الوصف',
-              filled: true,
-              fillColor: Colors.white,
-            ),
-          ),
-          const SizedBox(height: 40),
-          TextField(
-            controller: projectLinkController,
-            decoration: const InputDecoration(
-              hintText: 'رابط مشروع قيت هب',
-              filled: true,
-              fillColor: Colors.white,
-            ),
-          ),
-          DropdownButton<String>(
-            items: <String>['Flutter', 'Swift', 'UI / UX'].map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(value: value, child: Text(value));
-            }).toList(),
-            value: _selectedOption,
-            hint: const Text('Select an option'),
-            onChanged: (newValue) {
-              setState(() {
-                _selectedOption = newValue!;
-              });
-              print(newValue);
-            },
-            style: const TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.normal),
-            icon: const Icon(
-              Icons.arrow_drop_down,
-            ),
-          ),
-          const SizedBox(height: 40),
-          ElevatedButton(
-            onPressed: pickVideo,
-            child: const Text('تحميل مقطع الفيديو'),
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              print(_selectedOption);
-              final project = AddNewProject(
-                pId: _selectedOption,
-                pName: nameController.text,
-                pDescription: descriptionController.text,
-                gitHubLink: projectLinkController.text,
-              );
-              final response = await supabase.from('newProject').insert(
-                [
-                  project.toJson(),
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: DropdownButtonHideUnderline(
+                      child: DropdownButton2(
+                        items: <String>['Flutter', 'Swift', 'UI / UX']
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                              value: value, child: Text(value));
+                        }).toList(),
+                        value: _selectedOption,
+                        onChanged: (newValue) {
+                          setState(() {
+                            _selectedOption = newValue!;
+                          });
+                          print(newValue);
+                        },
+                        style: const TextStyle(
+                            color: Colors.blue,
+                            fontSize: 16,
+                            fontWeight: FontWeight.normal),
+                        isExpanded: true,
+                        buttonStyleData: ButtonStyleData(
+                          height: 40,
+                          width: 200,
+                          padding: const EdgeInsets.only(left: 14, right: 14),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              border: Border.all(color: Colors.black26),
+                              borderRadius: BorderRadius.circular(14)),
+                          elevation: 2,
+                        ),
+                        iconStyleData: const IconStyleData(
+                            icon: Icon(Icons.keyboard_arrow_down_rounded),
+                            iconDisabledColor: Colors.grey,
+                            iconEnabledColor: Colors.red,
+                            iconSize: 20),
+                        dropdownStyleData: DropdownStyleData(
+                          maxHeight: 200,
+                          width: 200,
+                          padding: null,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(14)),
+                          elevation: 8,
+                          offset: const Offset(-20, 0),
+                          scrollbarTheme: ScrollbarThemeData(
+                              thumbVisibility:
+                                  MaterialStateProperty.all<bool>(true),
+                              thickness: MaterialStateProperty.all<double>(6),
+                              radius: const Radius.circular(40)),
+                        ),
+                        menuItemStyleData: const MenuItemStyleData(
+                            height: 40,
+                            padding: EdgeInsets.only(left: 14, right: 14)),
+                      ),
+                    ),
+                  ),
+                  const CTTextFieldTittle('نوع المشروع'),
                 ],
-              );
+              ),
+              const CTTextFieldTittle('الإسم'),
+              const SizedBox(
+                width: 400,
+                child: AnimatedTextField(label: '.. اسم المشروع', suffix: null),
+              ),
+              const CTTextFieldTittle('الوصف'),
+              const SizedBox(
+                width: 400,
+                height: 200,
+                child: AnimatedTextField(label: '.. وصف المشروع', suffix: null),
+              ),
+              const CTTextFieldTittle(' Git Hub رابط المشروع على'),
+              const SizedBox(
+                width: 400,
+                child: AnimatedTextField(
+                  label: '.. Git Hub رابط ',
+                  suffix: null,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.all(14),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      height: 50,
+                      width: 160,
+                      child: MyButton(
+                        title: 'تحميل مقطع الفيديو',
+                        onTap: pickVideo,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                      width: 160,
+                      child: MyButton(
+                        title: 'اضف المشروع',
+                        onTap: () async {
+                          print(_selectedOption);
+                          final project = AddNewProject(
+                            pId: _selectedOption,
+                            pName: nameController.text,
+                            pDescription: descriptionController.text,
+                            gitHubLink: projectLinkController.text,
+                          );
+                          final response =
+                              await supabase.from('newProject').insert(
+                            [
+                              project.toJson(),
+                            ],
+                          );
 
-              await uploadVideoToSupabase();
-            },
-            child: const Text('اضف المشروع'),
+                          await uploadVideoToSupabase();
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              // Container(
+              //   child: ElevatedButton(
+              //     onPressed: pickVideo,
+              //     child: const Text('تحميل مقطع الفيديو'),
+              //   ),
+              // ),
+
+              // ElevatedButton(
+              //   onPressed: () async {
+              //     print(_selectedOption);
+              //     final project = AddNewProject(
+              //       pId: _selectedOption,
+              //       pName: nameController.text,
+              //       pDescription: descriptionController.text,
+              //       gitHubLink: projectLinkController.text,
+              //     );
+              //     final response = await supabase.from('newProject').insert(
+              //       [
+              //         project.toJson(),
+              //       ],
+              //     );
+
+              //     await uploadVideoToSupabase();
+              //   },
+              //   child: const Text('اضف المشروع'),
+              // ),
+            ],
           ),
         ],
       ),
